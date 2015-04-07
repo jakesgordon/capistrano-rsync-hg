82973617f29ac1ac654c4a832e48c9790351180d
Jake Gordon <jake@codeincomplete.com>
2015-04-06 19:39:35 -0700

    Skeleton gem files and draft implementation

---
+++ lib/capistrano/rsync/version.rb
@@ -0,0 +1,13 @@
+module Capistrano
+  module Rsync
+    NAME        = "capistrano-rsync-hg"
+    VERSION     = "0.1.0"
+    AUTHORS     = [ "Jake Gordon" ]
+    EMAIL       = "jake@codeincomplete.com"
+    HOMEPAGE    = "http://codeincomplete.com/"
+    SUMMARY     = "Capistrano v3 deploy with rsync from a local Hg repository"
+    DESCRIPTION = "Deploy your application using Capistrano v3 without requiring SCM access on the server - checkout the repository locally (using Hg) and deploy with Rsync"
+    LICENSE     = "MIT"
+  end
+end
+
